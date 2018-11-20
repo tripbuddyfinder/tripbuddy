f4ae23bd1c0bf2bb6106e7876f88cf585eb81965
rjn2325 <32041362+rjn2325@users.noreply.github.com>
2018-11-20 17:52:13 +0530

    remaned controllers

---
+++ tripbuddy/src/org/app/controllers/homecontroller.java
@@ -0,0 +1,46 @@
+package org.app.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.app.models.userModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class homecontroller {
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+
+	
+	public @ResponseBody userModel insertdata(@ModelAttribute("guesthome") 
+			userModel usr)
+	{
+
+		System.out.println(usr.toString());
+		System.out.println("Inserting data");
+
+		org.app.daos.UserDaoImpl dao = new org.app.daos.UserDaoImpl();
+		dao.insert(usr);
+		System.out.println("Inserted successfully");
+		System.out.println();
+		return usr;
+	}
+	@RequestMapping(value="hello", method=RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
+}
+
+//public @ResponseBody userModel showData() 
+//	return null;
+//	usr.setU_name("rjn");
+//	usr.setU_city("Delhi");
+//	usr.setU_email("@google.com");
+	
+//}
