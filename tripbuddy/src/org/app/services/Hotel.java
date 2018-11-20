d949345090918f9c0a82ad57d215a14aa32a52a5
rjn2325 <32041362+rjn2325@users.noreply.github.com>
2018-11-20 15:44:44 +0530

    first commit from rjn

--- tripbuddy/src/org/app/services/Hotel.java
+++ tripbuddy/src/org/app/services/Hotel.java
@@ -12,7 +12,7 @@ public class Hotel {
 	public String getHotels(String location)
 	{
 		HttpsURLConnection con=null;
-						
+		System.out.println("helllo");				
 		String https_url = "https://api.darksky.net/forecast/c437f82b23e0e23d5e2e91891aa443f9/"+geocode.getLatt()+","+geocode.getLongt()+","+time+"T12:00:00?exclude=currently,hourly,flags";
 	    URL url;
 	      try {
