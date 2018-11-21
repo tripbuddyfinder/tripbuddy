9dd646a79f0767807b2787c861c2b45020aa2d8a
rjn2325 <32041362+rjn2325@users.noreply.github.com>
2018-11-22 00:10:14 +0530

    checking

--- tripbuddy/src/org/app/controllers/registerController.java
+++ tripbuddy/src/org/app/controllers/registerController.java
@@ -4,7 +4,6 @@ import org.app.daos.UserDaoImpl;
 import org.app.models.userModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
