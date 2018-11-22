a961e610926e673f91090421c6eaaeda23217792
tripbuddyfinder <44338612+tripbuddyfinder@users.noreply.github.com>
2018-11-22 16:28:33 +0530

    Update hotelController.java

--- tripbuddy/src/org/app/controllers/hotelController.java
+++ tripbuddy/src/org/app/controllers/hotelController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-public class hotelcontroller
+public class hotelController
 { 
 	@RequestMapping(value="/addhotels" , method=RequestMethod.GET)
 	public String showhotellink()
