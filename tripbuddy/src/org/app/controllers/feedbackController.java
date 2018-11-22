613731d27adca376017acf2fec1ca4a64dcedf6c
rahul <rahul@rahul>
2018-11-22 17:18:59 +0530

    pushing laest changes

--- tripbuddy/src/org/app/controllers/feedbackController.java
+++ tripbuddy/src/org/app/controllers/feedbackController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class FeedbackController 
+public class feedbackController 
 {
 	@RequestMapping(value="/Feedback" ,method=RequestMethod.GET)
 	public String showFeedback()
---
+++ tripbuddy/src/org/app/services/userService.java
@@ -0,0 +1,5 @@
+package org.app.services;
+
+public class userService {
+
+}
