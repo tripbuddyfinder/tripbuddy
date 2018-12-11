ac2b44ef91568229d059f350a4a40a56e0f83ef8
rahul <rahul@rahul>
2018-12-11 17:20:38 +0530

    updated timestamp

--- tripbuddy/WebContent/resources/scripts/chat.js
+++ tripbuddy/WebContent/resources/scripts/chat.js
@@ -132,12 +132,14 @@ function removeSpace(String){
       }
       
       function tConv24(time24) {
-    	  var ts = time24;
+    	  var ts = time24;    	  
     	  var H = +ts.substr(0, 2);
+    	  var m = ts.substr(3, 4);
     	  var h = (H % 12) || 12;
     	  h = (h < 10)?("0"+h):h;  // leading 0 at the left for 1 digit hours
+    	  m = (m < 10)?("0"+m):m;
     	  var ampm = H < 12 ? " AM" : " PM";
-    	  ts = h + ts.substr(2, 3) + ampm;
+    	  ts = h +':'+ m + ampm;
     	  return ts;
     	};
 
