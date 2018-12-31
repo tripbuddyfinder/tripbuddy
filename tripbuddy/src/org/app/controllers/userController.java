e5f967d9e12bbd4df1f6c8d464cae48f2c995970
tripbuddyfinder <44338612+tripbuddyfinder@users.noreply.github.com>
2018-12-31 18:43:54 +0530

    Update userController.java
    update userController

--- tripbuddy/src/org/app/controllers/userController.java
+++ tripbuddy/src/org/app/controllers/userController.java
@@ -1,10 +1,15 @@
 package org.app.controllers;
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.app.models.userModel;
 import org.app.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +22,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class userController 
 {
 	UserService service = new UserService();
-	
+	//user
 	@RequestMapping(value = "/checkuser", method = RequestMethod.POST)
     public @ResponseBody String checkUser(@RequestBody userModel user, RedirectAttributes redirectAttr,Model model) {
 		String ok = "auth";
@@ -48,5 +53,18 @@ public class userController
 		}
 		return error;
     }
+	
+	
 		
-}
+		
+	@RequestMapping(value="/fetchusers",method=RequestMethod.GET) 
+	public String getuserlist(Model model,HttpSession session)
+	{
+		List<userModel> userlist=service.fetchUsers();
+		model.addAttribute("userlist", userlist);
+		System.out.println(userlist);
+		return "ShowUsers";
+	}
+	}
+		
+
