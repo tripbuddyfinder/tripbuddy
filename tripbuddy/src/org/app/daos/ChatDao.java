02df92a26b72847bdffe90e52591b95d358e2de1
tripbuddyfinder <44338612+tripbuddyfinder@users.noreply.github.com>
2019-01-01 19:33:12 +0530

    Update ChatDao.java

--- tripbuddy/src/org/app/daos/ChatDao.java
+++ tripbuddy/src/org/app/daos/ChatDao.java
@@ -3,10 +3,12 @@ package org.app.daos;
 import java.util.List;
 
 import org.app.models.ChatMessage;
+import org.app.models.HotelModel;
 
 public interface ChatDao {
 
 	public int saveMsg(ChatMessage msg) ;
 	public List<ChatMessage> getMsgs(String gid);
+	public List<HotelModel> getHotels(String gid);
 	
 }
